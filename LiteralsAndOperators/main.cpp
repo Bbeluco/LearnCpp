94315210605264d835b86ed486efcaa39bc42fc1
bbeluco <beluco.bispo@hotmail.com>
2023-11-04 15:52:18 -0300

    Introduction to literals and operators

---
+++ LiteralsAndOperators/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int x{5}; //This is a literal value, this mean I'm set a constant value in the code
+    std::cout << x << '\n';
+
+    std::cout << 4 + 2 << '\n';
+
+    //Look how the same operator (operator-) can be used in 2 cases, unary and binary cases
+    std::cout << -5 << '\n';
+    std::cout << 5 - 2 << '\n';
+
+    int a,b;
+    a = b = 5;
+
+    2 * 3;
+    std::cout << a;
+    return 0;
+}
