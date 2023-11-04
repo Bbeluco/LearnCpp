3d4c8733d1cc5fda7f2230328acca0d28220457d
bbeluco <beluco.bispo@hotmail.com>
2023-11-04 14:53:28 -0300

    Keywords and naming identifiers

---
+++ KeywordsAndConventions/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int today{4}; //Convention way to declare vars
+    int my_function(); //It is common to find C++ functions written in snake_case
+    int mySecondFunction(); //camelCase also is correct
+    int _name{}; //This should be avoid bc some OS/compilers use vars like this\
+    //The more I use the variable better its name should be
+    return 0;
+}
+
