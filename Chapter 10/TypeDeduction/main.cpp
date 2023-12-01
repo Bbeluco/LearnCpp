bae71bcfda059a4098f2750d2b3903a70fb41eb2
bbeluco <beluco.bispo@hotmail.com>
2023-12-01 16:06:40 -0300

    Type deduction for objects using the auto keyword

---
+++ Chapter 10/TypeDeduction/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <typeinfo>
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int main()
+{
+    auto a{3.3}; //auto its like any in typescript
+    auto b{add(1,1)};
+    constexpr auto c { 1 };
+
+    std::cout << typeid(a).name() << '\n';
+    std::cout << typeid(b).name() << '\n';
+    std::cout << typeid(c).name() << '\n';
+    return 0;
+}
