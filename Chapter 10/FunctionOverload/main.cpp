8f7dc7f0fdc224f0b8ef442708edd3b84c4220ec
bbeluco <beluco.bispo@hotmail.com>
2023-12-01 16:38:36 -0300

    Introduction to function overloading

---
+++ Chapter 10/FunctionOverload/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int add(int x, int y) {
+    return x+y;
+}
+
+int add(int x, int y) {
+    return x+y;
+}
+
+int main()
+{
+    std::cout << add(1,2) << '\n';
+    std::cout << add(1.2,2.5f) << '\n';
+    return 0;
+}
