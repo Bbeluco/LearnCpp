76d00519042b0dbb99098684d830c2e4a6b1334a
Bruno Beluco <beluco.bispo@hotmail.com>
2023-12-22 09:34:43 -0300

    Converting constructors and the explicit keyword

---
+++ Chapter 14/convertingConstructor/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+/*
+    Converting constructor means the code will do ONLY 1 conversion to access the required constructor
+    If the code needs more than 1 conversion to access the constructor the code won't compile
+*/
+
+class Employee {
+public:
+
+    /*
+    Explicit keyword ensures that constructor will not perform any converting
+    ONLY THE EXACT SAME TYPE REQUIRED BY THE CONSTRUCTOR IS NOW AVAILABLE  
+    */
+    explicit Employee(std::string_view& name)
+    : m_name{name}
+    {}
+    std::string_view getName() { return m_name; }
+
+private:
+    std::string m_name{};
+};
+
+std::string_view print(Employee e) {
+    std::cout << "Name: " << e.getName() << '\n';
+}
+
+int main(){
+    std::string_view t{"Bruno"};
+    print(Employee {t});
+    return 0;
+}
